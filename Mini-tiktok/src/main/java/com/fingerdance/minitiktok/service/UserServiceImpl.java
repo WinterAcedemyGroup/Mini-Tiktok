7ab829b181f792418960904571106cc6b8bbe722
joeCarf <qiao_ao@foxmail.com>
2023-01-24 21:38:25 +0800

    login & register

--- Mini-tiktok/src/main/java/com/fingerdance/minitiktok/service/UserServiceImpl.java
+++ Mini-tiktok/src/main/java/com/fingerdance/minitiktok/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.fingerdance.minitiktok.service;
 import com.fingerdance.minitiktok.mapper.UserRepository;
 import com.fingerdance.minitiktok.pojo.Response;
 import com.fingerdance.minitiktok.pojo.User;
+import com.fingerdance.minitiktok.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +18,21 @@ public class UserServiceImpl implements UserService{
         User query = userRepository.queryByName(username);
         if (query != null) {
             throw new Exception("该用户已存在");
-
         }
-
-
+        userRepository.add(username, SecurityUtils.encode(password));
         return null;
     }
 
-    @Override public Response login(String username, String password) {
+    @Override
+    public Response login(String username, String password) throws Exception{
+        User query = userRepository.queryByName(username);
+        if (query == null) {
+            throw new Exception("该用户不存在");
+        }
+        boolean passwordMatches = SecurityUtils.match(password, query.getPassWord());
+        if (!passwordMatches) {
+            throw new Exception("用户名或密码错误");
+        }
         return null;
     }
 }
